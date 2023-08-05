5393f4fe62a6529a9ae3cf45d437987154a173c5
Rustic Cyberpunk <32854305+cypnk@users.noreply.github.com>
2023-08-05 12:02:44 -0400

    Create install.nix.sh

---
+++ install.nix.sh
@@ -0,0 +1,35 @@
+#!/bin/sh
+
+# Helper script on *.nix create or backup the pub.db database in the /data folder
+
+
+# Data folder
+FD=data
+
+# Database name
+DB=$FD/pub.db
+
+# SQL File
+SQL=pub.db.sql
+
+# Log file
+LOG=$FD/install.log
+
+# Timestamp
+DATE=`date +%Y-%m-%d-%H-%M-%S`
+
+mkdir -p $FD
+touch $LOG
+
+# Backup to data folder, if pub.db already exists
+if [ -f $FD/$DB ]; then
+	sqlite3 $DB .dump > $DB.$DATE.sql
+ 	$MSG="- Backed up $DB $DATE"
+	echo $MSG >> $LOG
+ 	echo $MSG
+else
+	sqlite3 $DB < $SQL
+ 	$MSG="- Created $DB"
+ 	echo $MSG >> $LOG
+	echo $MSG
+fi
